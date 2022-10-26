82d9f6eb2052695cbb8f49c36e0110171176ec94
Aggie Skorska <57585879+agiskorska@users.noreply.github.com>
2022-10-26 14:24:39 +0100

    Add db config

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const mysql = require('mysql');
+
 const app = express()
 const port = 8000
+
+const db = mysql.createConnection({
+  host: "database-1.crygjk68zr6u.us-east-1.rds.amazonaws.com",
+  port: "3306",
+  user: "admin",
+  password: "qwerty123",
+  database: "mydb",
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
