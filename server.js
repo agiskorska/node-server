5554fd2f6694a63a12cf7d35c080cf13f1365ed7
agiskorska <skorskaga@gmail.com>
2022-10-27 06:54:43 +0100

    Add dependencies and database connection

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const mysql = require('mysql');
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 const port = 8000
-
+app.use(cors())
+app.use(bodyParser.json())
 const db = mysql.createConnection({
-  host: "database-1.crygjk68zr6u.us-east-1.rds.amazonaws.com",
+  host: "mysqldbtest.crygjk68zr6u.us-east-1.rds.amazonaws.com",
   port: "3306",
   user: "admin",
   password: "qwerty123",
@@ -26,6 +29,19 @@ app.get('/', (req, res) => {
 
 app.get('/dupa', (req, res) => {
   res.send('Hello dupa!')
+})
+
+app.post('/todb', (req, res) => {
+  console.log(req.body)
+  const firstName=req.body.firstName.toString()
+  const lastName=req.body.lastName.toString()
+  const email=req.body.email.toString()
+  const password=req.body.password.toString()
+  db.query("INSERT INTO `mydb`.`users` (firstName, lastName, email, password) VALUES ('"+firstName+"', '"+ lastName+"', '" + email+"', '" + password +"')", function(err, result){
+          if(err) throw err;
+          console.log("1 record inserted");
+        });
+  res.send('dupa');
 })
 
 app.listen(port, () => {
